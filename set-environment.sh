dfab7ee7f42b95e02633555fb4b0c76214721c1f
Haipeng LI <haipeng.li@imt-atlantique.net>
2019-03-12 19:04:11 +0100

    Add script to help set the environment

---
+++ set-environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+pip3 install numpy
+pip3 install pandas
+conda install scikit-learn
+pip3 install keras
+conda install -c conda-forge tensorflow 
+python -m pip install -U matplotlib
+
+echo "****************************************************************"
+echo "Congratulations! All the environment are installed successfully"
+echo "****************************************************************"
+echo "Now you enter the project!"
+echo "****************************************************************"
