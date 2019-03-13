a65403469c77b6a8491f0598b3daa31f58c4fa15
Li Haipeng <13688174653@163.com>
2019-03-13 13:44:31 +0100

    Update set-environment.sh

--- set-environment.sh
+++ set-environment.sh
@@ -6,6 +6,7 @@ pip3 install keras
 conda install -c anaconda seaborn 
 conda install -c conda-forge tensorflow 
 python -m pip install -U matplotlib
+conda install -c conda-forge lightgbm 
 
 echo "****************************************************************"
 echo "Congratulations! All the environment are installed successfully"
